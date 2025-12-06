0dd97d3a8579a2254eb6551e383c4e3be71ea50d
Matthieu Michon <matthieu.michon@gmail.com>
2025-12-06 22:16:00 +0100

    (day6.1) Add TAP dec/encoder in stub
    Simulation produces expected results.

--- 25/6/user_logic_stub.sv
+++ 25/6/user_logic_stub.sv
@@ -13,7 +13,36 @@ module user_logic (
     input wire update_dr
 );
 
-assign tdo = tdi;
+localparam int INBOUND_DATA_WIDTH = 8;
+typedef logic [INBOUND_DATA_WIDTH-1:0] data_t;
+data_t inbound_data;
+logic inbound_valid;
+
+tap_decoder #(.DATA_WIDTH(INBOUND_DATA_WIDTH)) tap_decoder_i (
+    // TAP signals
+        .tck(tck),
+        .tdi(tdi),
+        .test_logic_reset(test_logic_reset),
+        .ir_is_user(ir_is_user),
+        .shift_dr(shift_dr),
+        .update_dr(update_dr),
+    // Decoded signals
+        .data(inbound_data),
+        .valid(inbound_valid)
+);
+
+tap_encoder #(.DATA_WIDTH(INBOUND_DATA_WIDTH)) tap_encoder_i (
+    // TAP signals
+        .tck(tck),
+        .tdo(tdo),
+        .test_logic_reset(test_logic_reset),
+        .ir_is_user(ir_is_user),
+        .capture_dr(capture_dr),
+        .shift_dr(shift_dr),
+    // Encoded signals
+        .data(inbound_data),
+        .valid(inbound_valid)
+);
 
 endmodule
 `default_nettype wire
