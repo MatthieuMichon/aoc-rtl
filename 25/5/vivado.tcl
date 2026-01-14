6cc6c8360f02b54f5a3949424341f66dc5b90089
Matthieu Michon <matthieu.michon@gmail.com>
2026-01-14 11:00:59 +0100

    (2025day5.1) Use common vivado.tcl

--- 25/5/vivado.tcl
+++ 25/5/vivado.tcl
@@ -23,13 +23,12 @@ proc ::parse_named_arguments {arg_list} {
 }
 
 proc ::build {arg_dict} {
-    create_project -part xc7z020clg484-1 -in_memory
-    read_verilog -sv [glob ../*.sv]
+    create_project -part [dict get $arg_dict PART] -in_memory
+    read_verilog -sv [lsearch -all -inline -not [glob ../*.sv] *_tb.sv]
     read_xdc [glob ../*.xdc]
 
     set directive RuntimeOptimized; # speed-run the build process
-    synth_design -top shell \
-        -verilog_define USER_LOGIC_DEF=user_logic \
+    synth_design -top [lindex [find_top] 0] \
         -directive $directive \
         -flatten_hierarchy none \
         -debug_log -verbose
@@ -59,14 +58,16 @@ proc ::build {arg_dict} {
     report_disable_timing -user_disabled -file disable_timing.txt
     report_drc -no_waivers -file drc.txt
     report_exceptions -file exceptions.txt
+    ::xilinx::designutils::report_failfast -detailed_reports synth -file failfast.txt 
     report_high_fanout_nets -timing -load_types -max_nets 99 -file high_fanout_nets.txt
     report_methodology -no_waivers -file methodology.txt
     report_power -file power.txt
     report_qor_assessment -file qor_assessment.txt -full_assessment_details -quiet
     report_qor_suggestions -file qor_suggestions.txt -report_all_suggestions -quiet
+    report_ram_utilization -file ram_utilization.txt
     report_timing_summary -slack_lesser_than 20 -max_paths 1 -file timing_summary.txt; # lol
     report_utilization -file utilization.txt
-    catch {report_utilization -hierarchical -hierarchical_min_primitive_count 0 -file hierarchical_utilization.txt}
+    catch {report_utilization -hierarchical -hierarchical_min_primitive_count 0 -file utilization_hierarchical.txt}
 
     set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
     set_property BITSTREAM.CONFIG.USR_ACCESS TIMESTAMP [current_design]
@@ -74,9 +75,9 @@ proc ::build {arg_dict} {
     write_bitstream -force -logic_location_file -file fpga.bit
 }
 
-proc ::lint {} {
-    create_project -part xc7z020clg484-1 -in_memory
-    read_verilog -sv [glob ../*.sv]
+proc ::lint {arg_dict} {
+    create_project -part [dict get $arg_dict PART] -in_memory
+    read_verilog -sv [lsearch -all -inline -not [glob ../*.sv] *_tb.sv]
     synth_design -top [lindex [find_top] 0] \
         -lint -file lint.txt -debug_log -verbose
 }
@@ -99,44 +100,44 @@ proc ::load_inputs {arg_dict} {
     open_hw_target -jtag_mode on -quiet
 
     # set FPGA PL TAP IR to USER4
-    run_state_hw_jtag RESET; # this clears instruction register
-    run_state_hw_jtag IDLE
     set zynq7_ir_length 10; # must match FPGA device family / SLR count
     set zynq7_ir_user4 0x3e3; # same thing
+    run_state_hw_jtag RESET; # this clears instruction register
+    run_state_hw_jtag IDLE
     scan_ir_hw_jtag $zynq7_ir_length -tdi $zynq7_ir_user4
 
     # upload file contents
+    set zynq7_dr_length_byte 9; # zynq7: extra bit for ARM DAP bypass reg
     set new_line 0x0a; # `\n`
     set input_file [dict get $arg_dict INPUT_FILE]
-    set rotations [::textio::load ../$input_file]
+    set lines [::textio::load ../$input_file]
     puts -nonewline "Uploading bytes... "
     set bytes_uploaded 0
-    foreach rotation $rotations {
-        set len [string length $rotation]
+    foreach line $lines {
+        set len [string length $line]
         for {set i 0} {$i<$len} {incr i} {
-            scan [string index $rotation $i] %c char
-            set hex_char [format "%02x" $char]
-            scan_dr_hw_jtag 9 -tdi 0${hex_char}
-            run_state_hw_jtag IDLE; # run through state `UPDATE`
+            scan [string index $line $i] %c char
+            set hex_char [format "0x%02x" $char]
+            scan_dr_hw_jtag $zynq7_dr_length_byte -tdi $hex_char
             incr bytes_uploaded
         }
-        scan_dr_hw_jtag 9 -tdi $new_line
-        run_state_hw_jtag IDLE; # run through state `UPDATE`
+        scan_dr_hw_jtag $zynq7_dr_length_byte -tdi $new_line
         incr bytes_uploaded
     }
+    scan_dr_hw_jtag $zynq7_dr_length_byte -tdi $new_line
     puts "done. ($bytes_uploaded bytes)"
-
-    # cycle tck for purging data stuck between register stages
-    for {set i 0} {$i<500} {incr i} {
-        run_state_hw_jtag -state IDLE IDLE;
-    }
-
 }
 
-proc ::read_password {} {
-    run_state_hw_jtag IDLE
-    set password 0x[scan_dr_hw_jtag 16 -tdi 0]
-    puts "Password readback: [format %d $password] ($password)"
+proc ::read_result {} {
+    set result_width 16
+    set result 0x0
+    puts -nonewline "Waiting for non-zero result... "
+    while {$result == 0} {
+        set result 0x[scan_dr_hw_jtag $result_width -tdi 0]
+    }
+    puts "done."
+    puts "Result readback: [format %d $result] ($result)"
+    close_hw_target -quiet
 }
 
 proc run {argv} {
@@ -146,7 +147,7 @@ proc run {argv} {
             ::build $arg_dict
             ::program
             ::load_inputs  $arg_dict
-            ::read_password
+            ::read_result
         }
         "build" {
             ::build $arg_dict
@@ -155,11 +156,12 @@ proc run {argv} {
             ::program
         }
         "run" {
+            ::program
             ::load_inputs  $arg_dict
-            ::read_password
+            ::read_result
         }
         "lint" {
-            ::lint
+            ::lint  $arg_dict
         }
         default {
             ::build $arg_dict
