eb3c550e1dc4f97f730f3230c51b063d96361919
Matthieu Michon <matthieu.michon@gmail.com>
2026-02-10 13:39:55 +0100

    (2015day6.1) sv: Add sanity check on position values

--- 15/6-1/light_display.sv
+++ 15/6-1/light_display.sv
@@ -17,18 +17,59 @@ module light_display #(
         output logic [RESULT_WIDTH-1:0] count_value
 );
 
-assign instr_ready = 1'b1;
+localparam int OPERATION_WIDTH = 2;
+localparam int POSITION_WIDTH = 12;
 
-always_ff @(posedge clk) begin: dummy
+typedef logic [OPERATION_WIDTH-1:0] operation_t;
+typedef logic [POSITION_WIDTH-1:0] position_t;
+typedef enum operation_t {
+    TURN_OFF = 2'b00,
+    TOGGLE = 2'b01,
+    TURN_ON = 2'b11
+} operations_t;
+
+logic last, valid;
+operation_t operation;
+position_t start_row, start_col, end_row, end_col;
+position_t row_count;
+
+assign {last, valid, operation, start_row, start_col, end_row, end_col} = instr_data;
+
+always_ff @(posedge clk) begin: dummy_flow_control
+    if (reset) begin
+        instr_ready <= 1'b0;
+        row_count <= '0;
+    end else begin
+        if (instr_ready && instr_valid) begin
+            instr_ready <= 1'b0;
+            row_count <= end_row - start_row;
+        end else if (row_count > 0) begin
+            row_count <= row_count - 1;
+        end else begin
+            instr_ready <= 1'b1;
+        end
+    end
+end
+
+always @(posedge clk) begin
+    assert (!(instr_ready && instr_valid && (start_row > end_row)))
+        else $error("Unexpected start_row vs end_row");
+end
+
+always_ff @(posedge clk) begin: finish_condition
     if (reset) begin
         count_done <= 1'b0;
         count_value <= '0;
     end else begin
-        count_done <= instr_last;
         if (instr_ready && instr_valid) begin
-            count_value <= count_value + $countones(instr_data);
+            count_done <= instr_last;
+            count_value <= count_value + 1'b1;
         end
     end
 end
+
+wire _unused_ok = 1'b0 && &{1'b0,
+    last, valid, operation, start_col, end_col,
+    1'b0};
 
 endmodule
